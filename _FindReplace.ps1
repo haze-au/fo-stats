8b95c2de98d9e2e71ba8f63fee466b4e198a1179
haze-au <91508911+haze-au@users.noreply.github.com>
2023-02-06 12:44:10 +1100

    tools

---
+++ _FindReplace.ps1
@@ -0,0 +1,47 @@
+﻿$findReplace = @{}
+#$files = gci .\2022_T\220531_SF-!\*.json
+$files = gci '.\2022_T\_ALL\2022-05-01_10-38-53_`[turtler`]_blue_vs_red.json'
+
+
+#Hashtable items to replace
+$findReplace.'bentski' = 'bent'
+$findReplace.'sobrahbent' = 'bent'
+$findReplace.'sobrahzel' = 'zel'
+$findReplace.'‹sobrah‹zel' = 'zel'
+$findReplace.'‡s‡meht' = 'meht'
+$findReplace.'sobrahmeht' = 'meht'
+$findReplace.'\(1\)spas' = '!spas'
+$findReplace.'"spas"' = '"!spas"'
+$findReplace.'!hello' = '!pecan'
+$findReplace.'‰b‰lagfox' = '‰b‰redfox'
+$findReplace.'‡s‡meht' = 'meht'
+$findReplace.'seabo' = 'seano'
+$findReplace.'"shitpeas"' = '"seano"'
+$findReplace.'(1)shitpeas' = 'seano'
+$findReplace.'seanpeas' = 'seano'
+
+$findReplace.'"awm"' = '"‰b‰wm"'
+$findReplace.'"loddy"' = '"!lordy"'
+$findReplace.'"loddy"' = '"!lordy"'
+
+$files
+'---'
+foreach ($f in $files) {
+$fn = $f -replace '\[','`[' -replace '\]','`]'
+
+$txt = gc -Path ($fn) -Raw
+
+foreach ($fr in $findReplace.Keys) {
+  $txt = $txt -replace $fr,$findReplace.$fr
+}
+
+$txt | Out-File $fn
+
+}
+
+
+
+
+
+
+
---
+++ _Join-Fix-2xJSON.ps1
@@ -0,0 +1,16 @@
+﻿$strOut = ''
+
+foreach ($l in (gc '.\2022_T\220213_ZAF-BDE\unedited-swoop\2022-02-13_09-24-50_`[ff-swoop`]_blue_vs_red.json')) {
+  
+if ($l -match '"playerTeam": ([1-2])') {   $l = $l -replace """playerTeam"": [1-2]","""playerTeam"": $(Switch ($matches[1]) { 1 { '2' }; 2 { '1' } })" }
+if ($l -match '"targetTeam": ([1-2])') {   $l = $l -replace """targetTeam"": [1-2]","""targetTeam"": $(Switch ($matches[1]) { 1 { '2' }; 2 { '1' } })" }
+if ($l -match '"attackerTeam": ([1-2])') { $l = $l -replace """attackerTeam"": [1-2]","""attackerTeam"": $(Switch ($matches[1]) { 1 { '2' }; 2 { '1' } })" }
+if ($l -match '"time": ([0-9]{1,4})') {    $l = $l -replace """time"": $($matches[1])","""time"": $([int]$matches[1] + 600)" }
+
+$strOut += "$l`n"
+
+}
+
+
+$strOut | Out-File .\new.txt
+& .\new.txt
