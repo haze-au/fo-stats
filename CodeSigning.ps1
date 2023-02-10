47172cfa9288b8c71a80145c5cd1114542e2cbe6
haze-au <91508911+haze-au@users.noreply.github.com>
2023-02-10 21:23:22 +1100

    batch tweaks and self signed R2b

---
+++ CodeSigning.ps1
@@ -0,0 +1,43 @@
+﻿$authenticode = New-SelfSignedCertificate -Subject "Haze Authenticode" -CertStoreLocation Cert:\LocalMachine\My -Type CodeSigningCert
+
+#1a
+# Add the self-signed Authenticode certificate to the computer's root certificate store.
+## Create an object to represent the LocalMachine\Root certificate store.
+ $rootStore = [System.Security.Cryptography.X509Certificates.X509Store]::new("Root","LocalMachine")
+## Open the root certificate store for reading and writing.
+ $rootStore.Open("ReadWrite")
+## Add the certificate stored in the $authenticode variable.
+ $rootStore.Add($authenticode)
+## Close the root certificate store.
+ $rootStore.Close()
+ 
+#1bSKIPPED
+# Add the self-signed Authenticode certificate to the computer's trusted publishers certificate store.
+## Create an object to represent the LocalMachine\TrustedPublisher certificate store.
+ $publisherStore = [System.Security.Cryptography.X509Certificates.X509Store]::new("TrustedPublisher","LocalMachine")
+## Open the TrustedPublisher certificate store for reading and writing.
+ $publisherStore.Open("ReadWrite")
+## Add the certificate stored in the $authenticode variable.
+ $publisherStore.Add($authenticode)
+## Close the TrustedPublisher certificate store.
+ $publisherStore.Close()
+
+ #2
+ # Confirm if the self-signed Authenticode certificate exists in the computer's Personal certificate store
+ Get-ChildItem Cert:\LocalMachine\My | Where-Object {$_.Subject -eq "CN=Haze Authenticode"}
+# Confirm if the self-signed Authenticode certificate exists in the computer's Root certificate store
+ Get-ChildItem Cert:\LocalMachine\Root | Where-Object {$_.Subject -eq "CN=Haze Authenticode"}
+# Confirm if the self-signed Authenticode certificate exists in the computer's Trusted Publishers certificate store
+ Get-ChildItem Cert:\LocalMachine\TrustedPublisher | Where-Object {$_.Subject -eq "CN=Haze Authenticode"}
+
+ #3
+ # Get the code-signing certificate from the local computer's certificate store with the name *ATA Authenticode* and store it to the $codeCertificate variable.
+$codeCertificate = Get-ChildItem Cert:\LocalMachine\My | Where-Object {$_.Subject -eq "CN=Haze Authenticode"}
+
+# Sign the PowerShell script
+# PARAMETERS:
+# FilePath - Specifies the file path of the PowerShell script to sign, eg. C:\ATA\myscript.ps1.
+# Certificate - Specifies the certificate to use when signing the script.
+# TimeStampServer - Specifies the trusted timestamp server that adds a timestamp to your script's digital signature. Adding a timestamp ensures that your code will not expire when the signing certificate expires.
+Set-AuthenticodeSignature -FilePath H:\FO_Stats_with_downloader_R2\_FoDownloader.ps1 -Certificate $codeCertificate -TimeStampServer http://timestamp.digicert.com
+Set-AuthenticodeSignature -FilePath H:\FO_Stats_with_downloader_R2\FO_stats_v2.ps1 -Certificate $codeCertificate -TimeStampServer http://timestamp.digicert.com
