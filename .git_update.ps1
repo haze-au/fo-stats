8c56b8a37e10e8c058d1647eea5d410a1dada720
haze-au <91508911+haze-au@users.noreply.github.com>
2023-02-23 16:03:24 +1100

    .git_update.ps1

---
+++ .git_update.ps1
@@ -0,0 +1,8 @@
+$backupDir = New-Item ".\.backup_$('{0:yyyy-MM-dd-hh-mm-ss}' -f (Get-Date))" -ItemType Directory
+
+foreach ($f in @('FO_stats_v2.ps1','FO_stats_daily.ps1','_FoDownloader.ps1')) {
+
+Copy-Item $f $backupDir
+(Invoke-WebRequest https://raw.githubusercontent.com/haze-au/fo-stats/main/$f -Headers @{"Cache-Control"="no-cache"}).Content | Out-File $f
+
+}
