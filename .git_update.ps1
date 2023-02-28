1ed0c72e72afbe733ee4ef361422e3db5cf22214
haze-au <91508911+haze-au@users.noreply.github.com>
2023-02-28 15:24:04 +1100

    css tweak

--- .git_update.ps1
+++ .git_update.ps1
@@ -1,6 +1,6 @@
 $backupDir = New-Item ".\.backup_$('{0:yyyy-MM-dd-hh-mm-ss}' -f (Get-Date))" -ItemType Directory
 
-foreach ($f in @('FO_stats_v2.ps1','FO_stats_join-json.ps1','_FoDownloader.ps1')) {
+foreach ($f in @('FO_stats_v2.ps1','FO_stats_join-json.ps1','_FoDownloader.ps1','.fo_stats.css')) {
 
 Copy-Item $f $backupDir
 (Invoke-WebRequest https://raw.githubusercontent.com/haze-au/fo-stats/main/$f -Headers @{"Cache-Control"="no-cache"}).Content | Out-File $f
