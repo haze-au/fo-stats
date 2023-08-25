5f6c56a61e4f25264f0e70046c46503dddf25047
haze-au <91508911+haze-au@users.noreply.github.com>
2023-08-25 11:34:30 +1000

    bugs

--- FO_stats_v2.ps1
+++ FO_stats_v2.ps1
@@ -2339,6 +2339,7 @@ foreach ($jsonFile in $inputFile) {
     }
 
     arrSummaryTable-SetPlayerProperty -table ([ref]$arrSummaryAttTable) -player $p -property 'SGKills'  -value (($arrPlayerTable | Where Name -EQ $p | Measure SGKills -Sum).Sum)
+    arrSummaryTable-SetPlayerProperty -table ([ref]$arrSummaryAttTable) -player $p -property 'SGDeath'  -value (($arrPlayerTable | Where Name -EQ $p | Measure SGDeath -Sum).Sum)
     arrSummaryTable-SetPlayerProperty -table ([ref]$arrSummaryAttTable) -player $p -property 'FlagCap'  -value (($arrPlayerTable | Where Name -EQ $p | Measure FlagCap -Sum).Sum)
     arrSummaryTable-SetPlayerProperty -table ([ref]$arrSummaryAttTable) -player $p -property 'FlagTake' -value (($arrPlayerTable | Where Name -EQ $p | Measure FlagTake -Sum).Sum)
     arrSummaryTable-SetPlayerProperty -table ([ref]$arrSummaryAttTable) -player $p -property 'FlagTime' -value (($arrPlayerTable | Where Name -EQ $p | Measure FlagTime -Sum).Sum)
